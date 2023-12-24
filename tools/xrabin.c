05a7428db9b0b9806edab4171845384148acae66
Davide Libenzi <davide.libenzi@gmail.com>
2023-12-24 06:58:45 +0100

    License migration.

--- tools/xrabin.c
+++ tools/xrabin.c
@@ -1,33 +1,19 @@
-/*
- *  xrabin by Davide Libenzi (Rabin's polynomial generator)
- *  Copyright (C) 2006  Davide Libenzi
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  Davide Libenzi <davidel@xmailserver.org>
- *
- *
- *  Hints, ideas and code for the implementation came from:
- *
- *  Rabin's original paper: http://www.xmailserver.org/rabin.pdf
- *  Chan & Lu's paper:      http://www.xmailserver.org/rabin_impl.pdf
- *  Broder's paper:         http://www.xmailserver.org/rabin_apps.pdf
- *  LBFS source code:       http://www.fs.net/sfswww/lbfs/
- *  Geert Bosch's post:     http://marc.theaimsgroup.com/?l=git&m=114565424620771&w=2
- *
+/*    Copyright 2023 Davide Libenzi
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ * 
  */
+
 
 #include <stdio.h>
 #include <stdlib.h>
